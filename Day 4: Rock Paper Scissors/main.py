4257cb24dc17a30b51f48404f5de38ece1900069
Aakanksha Khindri <aakanksha.khindri@gmail.com>
2022-12-22 22:55:58 +0530

    play rock paper scissors

--- Day 4: Rock Paper Scissors/main.py
+++ Day 4: Rock Paper Scissors/main.py
@@ -32,7 +32,7 @@ game = [rock, paper, scissors]
 choice = int(input("What do you choose?\n Type 0 for rock, 1 for Paper, and 2 for Scissors.\n"))
 
 print("You chose:" + game[choice])
-0
+
 
 comp_choice = random.randint(0,2)
 print("Computer chose:")
@@ -41,19 +41,19 @@ print(game[comp_choice])
 # game rules, deciding who wins 
 #user wins
 if choice == 0 and comp_choice == 2: #a- rock, b- scissors
-    print("You win")
+    print("You win!")
 elif choice == 1 and comp_choice == 0: #a- paper, b-rock
-    print("You win")
+    print("You win!")
 elif choice == 2 and comp_choice == 1: # a-scissors, b-paper
-    print("You win")
+    print("You win!")
 #user loses
 if choice == 2 and comp_choice == 0: #b- rock, a- scissors
-    print("You lose")
+    print("You lose!")
 elif choice == 0 and comp_choice == 1: #b- paper, a-rock
-    print("You lose")
+    print("You lose!")
 elif choice == 1 and comp_choice == 2: # b-scissors, a-paper
-    print("You lose")
+    print("You lose!")
 elif choice == comp_choice:
-    print("It's a draw") #draw
+    print("It's a draw!") #draw
 else:
     print("You chose an invalid number, you lose!")
